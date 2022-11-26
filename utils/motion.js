02e16f7dffc779e7dc061cde6c630282ec4968d9
AmbienceStudios <ambiencexyz@gmail.com>
2022-11-25 19:35:26 -0800

    mobile motion

--- utils/motion.js
+++ utils/motion.js
@@ -22,7 +22,7 @@ export const navVariants = {
 export const slideIn = (direction, type, delay, duration) => ({
   hidden: {
     x: direction === 'left' ? '-100%' : direction === 'right' ? '100%' : 0,
-    y: direction === 'up' ? '-200%' : direction === 'down' ? '100%' : 0,
+    y: direction === 'up' ? '-400%' : direction === 'down' ? '100%' : 0,
   },
   show: {
     x: 0,
