3336166cf34caa7e405ee2c3532e35e7ad8bc8c0
AmbienceStudios <ambiencexyz@gmail.com>
2022-11-26 15:19:02 -0800

    Added Program Pages

---
+++ pages/Track.js
@@ -0,0 +1,24 @@
+'use client';
+
+import { Footer, Navbar } from '../components';
+// eslint-disable-next-line import/named
+import { GearShop } from '../sections';
+
+const Track = () => (
+  <div className="bg-black overflow-hidden">
+    <Navbar />
+    <div className="relative">
+      <GearShop />
+      <div className="gradient-03 z-0" />
+    </div>
+    <div className="relative">
+      <div className="gradient-04 z-0" />
+    </div>
+    <div className="relative">
+      <div className="gradient-04 z-0" />
+    </div>
+    <Footer />
+  </div>
+);
+
+export default Track;
