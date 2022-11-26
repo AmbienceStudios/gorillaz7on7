7176b616b16670d548d688e77666c46b5d396f43
AmbienceStudios <ambiencexyz@gmail.com>
2022-11-26 15:06:33 -0800

    Fixed Next.js errors

--- pages/GorillazGear.js
+++ pages/GorillazGear.js
@@ -1,33 +1,31 @@
 'use client';
 
 import { Footer, Navbar } from '../components';
-// eslint-disable-next-line import/named
-import {  Explore, Feedback, Hero, Insights } from '../sections';
 import GearShop from '../sections/GearShop';
 
 const GorillazGear = () => (
   <div className="bg-black overflow-hidden">
-  <Navbar />
-  {/* <Hero /> */}
-  <div className="relative">
-    <GearShop />
-    <div className="gradient-03 z-0" />
-    {/* <Explore /> */}
-  </div>
-  <div className="relative">
-    {/* <GetStarted /> */}
-    <div className="gradient-04 z-0" />
-    {/* <WhatsNew /> */}
-  </div>
-  {/* <World /> */}
-  <div className="relative">
+    <Navbar />
+    {/* <Hero /> */}
+    <div className="relative">
+      <GearShop />
+      <div className="gradient-03 z-0" />
+      {/* <Explore /> */}
+    </div>
+    <div className="relative">
+      {/* <GetStarted /> */}
+      <div className="gradient-04 z-0" />
+      {/* <WhatsNew /> */}
+    </div>
+    {/* <World /> */}
+    <div className="relative">
 
-    {/* <Insights /> */}
-    <div className="gradient-04 z-0" />
-    {/* <Feedback /> */}
+      {/* <Insights /> */}
+      <div className="gradient-04 z-0" />
+      {/* <Feedback /> */}
+    </div>
+    <Footer />
   </div>
-  <Footer />
-</div>
 );
 
 export default GorillazGear;
